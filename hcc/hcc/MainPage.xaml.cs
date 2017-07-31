689142a1a311803dea9eb59e7f4abe7ae528941a
Helmut Theis <helmut.theis@hypermediasystems.de>
2017-07-31 15:28:34 +0200

    Projektdateien hinzufügen.

---
+++ hcc/hcc/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+
+namespace hcc
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void tbGet_Clicked(object sender, EventArgs e)
+        {
+            string url = tbUrl.Text.Trim();
+            tbResult.Text = "Loading " + url + "...";
+            HttpClient hc = new HttpClient();
+            try
+            {
+                string responseBodyAsText = await hc.GetStringAsync(url);
+                tbResult.Text = responseBodyAsText;
+
+            }
+            catch (Exception ex)
+            {
+                tbResult.Text = ex.Message + Environment.NewLine + ex.InnerException.Message;
+            }
+
+        }
+
+
+        private void tbReload_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
